b2848e9bdf92d65e877f5581874c1de20101b7bf
Chris Gregory <chrisgregory418@gmail.com>
2014-08-15 15:08:05 -0600

    commit

--- src/HuffmanCompressor.java
+++ src/HuffmanCompressor.java
@@ -1,11 +1,13 @@
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 
 public class HuffmanCompressor {
 	public Byte[] compress(HuffmanTree tree, Byte[] byteArray) {
-		tree = new HuffmanTree(byteArray);
+		//For each byte in the Array, shove it through the tree and get it's new encoding.
 		List<Byte> newByteList = new ArrayList<Byte>();
 		Bits bits = new Bits();
+		
 		for (byte b : byteArray) {
 			tree.fromByte(b, bits);
 		}
@@ -25,27 +27,51 @@ public class HuffmanCompressor {
 		return result;
 	}
 
-	public byte[] decompress(HuffmanTree tree, int uncompressedLength, Byte[] byteArray) {
-		//OKAY Temporarily forget everything below here:
+	public byte[] decompress(HuffmanTree tree, int uncompressedLength,
+			Byte[] byteArray) {
+		System.out.println("Decompressing Byte[].");
+		System.out.println("Compressed Message ByteCount: " + byteArray.length);
+		// For each byte,
+		// Get each byte's value from the huffman tree.
+		// Compose the new values.
+
 		List<Byte> newByteList = new ArrayList<Byte>();
 		Bits bits = new Bits();
+		//Add each byte to bits.
 		for (Byte b : byteArray) {
-			String s = b.toString();
-			int i = Integer.parseInt(s, 2);//HOW DO I HANDLE NEGATIVE NUMBERS?
-			System.out.println();
+			boolean[] bitsInByte = bits(b);
+			b = Byte.valueOf(Integer.toString(b.intValue()));
+			for (int i = 0; i < 8; i++) {
+				bits.add(bitsInByte[i]);
+			}
 		}
-		while (bits.size() > 0) {
-			Byte newByte = tree.toByte(bits);
-			newByteList.add(newByte);
+		System.out.println("ByteCount = " + bits.size() / 8); //This is 52389 with JC's thing.
+		//Get the codes using bits.
+		while(bits.size() > 0){
+			newByteList.add(tree.toByte(bits));
 		}
-		Byte[] result = new Byte[newByteList.size()];
-		result = newByteList.toArray(result);
-		byte[] resultbytes = new byte[result.length];
+		System.out.println("ByteCount = " + newByteList.size()); //This is 55035
+		
+		// Bits now represents byteArray after huffman decoding.
+		System.out.println("NewByteListBuilt.");
+		System.out.println("Transferring from Bytes to bytes.");
+		byte[] resultbytes = new byte[newByteList.size()];
 		int i = 0;
-		for (Byte b : result) {
+		for (Byte b : newByteList) {
 			resultbytes[i++] = b;
 		}
+		System.out.println("Returning decompressed Byte Array.");
+		System.out.println("Decompressed ByteCount: " + resultbytes.length);
 		return resultbytes;
 	}
-
+	
+	static boolean[] bits(byte b) {
+		  int n = 8;
+		  final boolean[] set = new boolean[n];
+		  while (--n > 0) {
+		    set[n] = (b & 0x80) == 0;
+		    b <<= 1;
+		  }
+		  return set;
+		}
 }
